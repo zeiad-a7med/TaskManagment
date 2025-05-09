74d080c1dd628491d78133856b2a505f511693a8
zeiad-a7med <zeiadahmed194@gmail.com>
2025-05-09 15:10:54 +0300

    fix

--- TaskManagment/TaskManagmentApp.swift
+++ TaskManagment/TaskManagmentApp.swift
@@ -7,6 +7,7 @@
 
 import SwiftUI
 import SwiftData
+import FBSDKCoreKit
 
 @main
 struct TaskManagmentApp: App {
@@ -21,11 +22,30 @@ struct TaskManagmentApp: App {
             fatalError("Could not create ModelContainer: \(error)")
         }
     }()
+    @UIApplicationDelegateAdaptor(AppDelegate.self) var appDelegate
 
     var body: some Scene {
         WindowGroup {
             ContentView()
         }
         .modelContainer(sharedModelContainer)
+    }
+}
+class AppDelegate: NSObject, UIApplicationDelegate {
+    func application(
+        _ application: UIApplication,
+        didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]? = nil
+    ) -> Bool {
+        Settings.shared.appID = "1181677730375333" // Debug
+        ApplicationDelegate.shared.application(application, didFinishLaunchingWithOptions: launchOptions)
+        return true
+    }
+
+    func application(
+        _ app: UIApplication,
+        open url: URL,
+        options: [UIApplication.OpenURLOptionsKey: Any] = [:]
+    ) -> Bool {
+        return ApplicationDelegate.shared.application(app, open: url, options: options)
     }
 }
